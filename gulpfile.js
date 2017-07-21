fe9d280638a857b1cc7560c06a8202ce17e00a8b
glennton <create@glennroman.com>
2017-07-20 23:37:06 -0700

    First Commit

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+'use strict';
+const gulp = require('gulp');
+const jade = require('gulp-jade');
+const sass = require('gulp-sass');
+const babel = require('gulp-babel');
+const watch = require('gulp-watch');
+const concat = require('gulp-concat')
+
+//Default Command
+gulp.task('default',[],()=>{
+
+})
