89e71f24931c27b5072d0ed47b50d792b829c12d
glennton <create@glennroman.com>
2017-08-08 13:30:00 -0700

    New HTML Object, extends floating object

---
+++ public/src/assets/scripts/htmlObj.js
@@ -0,0 +1,22 @@
+import {floatObj} from '../../assets/scripts/floatObj.js';
+
+export class htmlObj extends floatObj {
+  constructor(src, parent, name, options) {
+    super(parent, name, options)
+    this.src = src;
+    this.color = options['data-color'] || "#000000";
+  }
+
+  _setSyles(){
+    this.element['style']['fill'] = this.color
+    this.element['style']['color'] = this.color
+    super._setSyles()
+  }
+
+  make(e, stage){
+    //Place svg in parent
+    const svgElement = document.createRange().createContextualFragment(this.src);
+    e.appendChild(svgElement)
+    super.make(e, stage)
+  }
+}
