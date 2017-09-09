cc73df388d8b401a9dade83d79ba53c590372b2c
glennton <create@glennroman.com>
2017-09-09 11:49:57 -0700

    Positioning from center of objects, rotate and x tweening.

--- public/src/assets/scripts/floatObj.js
+++ public/src/assets/scripts/floatObj.js
@@ -18,22 +18,31 @@ export class floatObj {
     this.mouseDepth     = parseInt(options['data-mouse-depth']) || 0;
     this.rotate         = options['data-rotate'] || 0;
     this.pDepth         = options['data-p-depth'] || 1;
-    this.pStart         = parseInt(options['data-pstart']) || .1;
-    this.pEnd           = parseInt(options['data-pend']) || null;
-    this.pStartY        = parseInt(options['data-pstarty']) || 0;
-    this.pEndY          = parseInt(options['data-pendy']) || 100;
-    this.pStartX        = parseInt(options['data-pstartx']) || 0;
-    this.pEndX          = parseInt(options['data-pendx']) || 0;
+    this.pStart         = parseInt(options['data-pstart']) || 0;
+    this.pEnd           = parseInt(options['data-pend']) || 0;
+    this.pEndY          = parseInt(options['data-pendy']) || null;
+    this.pEndX          = parseInt(options['data-pendx']) || null;
+    this.pEndR          = parseInt(options['data-pendr']) || null;
+    this.r              = parseInt(options['data-r']) || null;
+    this.yArcAmplitude  = parseInt(options['data-yarcamplitude']) || 0;
     //If initScaleH not defined, keep same proportion by setting to initScaleW
     this.floatFrequency = options['data-float-frequency'] || 0;
     this.floatAmplitude = options['data-float-amplitude'] || 0;
     this.floatAngle     = options['data-float-angle'] || 0;
-    //Not Set
+
+    if(this.pStart + this.pEnd != 0){
+      this.pActive = true
+    }
+
+    //Defaults / Not Set
     this.t;
     this.element;
     this.stage;
     this.plaxY = 0;
     this.plaxX = 0;
+    this.plaxR = 0;
+    this.yArc = 0;
+    this.intpl
   }
   // Make sprite and add to stage
   make(e, stage){
@@ -58,6 +67,10 @@ export class floatObj {
       this.t = this.initY[2]
       this.l = this.initX[2]
     }
+    $(this.element).css({
+      'margin-left': `-${ $(this.element).outerWidth()/2 }px`,
+      'margin-top': `-${ $(this.element).outerHeight()/2 }px`
+    })
   }
   _interpolate(start, end){
     let change = (end - start) / (this.pEnd - this.pStart )
@@ -65,18 +78,21 @@ export class floatObj {
   }
   //Update plax modifier if scrolled
   calcScroll(){
-    if(this.pStart != null && this.pEnd != null){
+    if(this.pActive){
       if(this.parent.interpolation > this.pStart && this.parent.interpolation < this.pEnd){
-        this.plaxY = this._interpolate(this.pStartY, this.pEndY)
-        this.plaxX = this._interpolate(this.pStartX, this.pEndX)
-      }else{
-        if(this.parent.interpolation < this.pStart){
-          this.plaxY = this.pStartY
-          this.plaxX = this.pStartX
+        if(this.pEndY){
+          this.plaxY = this._interpolate(0, this.pEndY)
         }
-        if(this.parent.interpolation > this.pEnd){
-          this.plaxY = this.pEndY
-          this.plaxX = this.pEndX
+        if(this.pEndX){
+          this.plaxX = this._interpolate(0, this.pEndX)
+        }
+        if(this.yArcAmplitude){
+          const angle = this._interpolate(0, Math.PI)
+          const amplitude = 10
+          this.yArc = (Math.sin(angle) * this.yArcAmplitude)
+        }
+        if(this.pEndR){
+          this.plaxR = this._interpolate(0, this.pEndX)
         }
       }
     }
@@ -85,8 +101,7 @@ export class floatObj {
   //Recalc Frame if mouse moved
   calcFrame(){
     //X Calc
-    let left =
-      this.l
+    let left = this.l
       + ((this.stage.mouseX - .5) * this.mouseDepth); // creates range -0.5 to +0.5
     left = left + this.plaxX
     //Y Calc
@@ -94,12 +109,15 @@ export class floatObj {
     top = top
       + this.stage.mouseY // Mouse modifier
       * this.mouseDepth;
+    //Rotate Calc
+    let rotate = this.r + this.plaxR
     //Plax Modifier
-    top = top + this.plaxY
+    top = top + this.plaxY - this.yArc
     //Proportion Modifier
     $(this.element).css({
       'left': `${left}%`,
-      'top': `${top}%`
+      'top': `${top}%`,
+      'transform': `rotate(${rotate}deg) translate3d(0,0,1px)`
     })
     //if(this.name == 'test'){console.log(this.plaxX)}
   }
